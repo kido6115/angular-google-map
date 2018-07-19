5cc79c8ca05c8b53d5b3400bf48d56c697f721b8
sungyeh <kido6115@hotmail.com>
2018-07-19 15:14:55 +0800

    新增line chart

--- src/app/polygon/polygon.component.ts
+++ src/app/polygon/polygon.component.ts
@@ -247,12 +247,59 @@ export class PolygonComponent implements OnInit {
       .attr("text-anchor", "end")
       .text("Frequency");
   }
+  lineChart(data: any[]) {
+
+    var svgWidth = 1500, svgHeight = 400;
+    var margin = { top: 20, right: 20, bottom: 30, left: 50 };
+    var width = svgWidth - margin.left - margin.right;
+    var height = svgHeight - margin.top - margin.bottom;
+    var svg = d3.select('#svg-linechart').append('svg')
+      .attr("width", svgWidth)
+      .attr("height", svgHeight);
+    var g = svg.append("g")
+      .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")"
+      );
+    var x = d3.scaleBand().rangeRound([0, width]);
+    var y = d3.scaleLinear().rangeRound([height, 0]);
+    var line = d3.line()
+      .x(function (d: any) { return x(d.key) })
+      .y(function (d: any) { return y(+d.value) })
+    x.domain(data.map(data => data.key));
+    y.domain(d3.extent(data, function (d: any) { return +d.value }));
+
+    g.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x))
+      .selectAll('text');
+
+    g.append("g")
+      .call(d3.axisLeft(y))
+      .append("text")
+      .attr("fill", "#000")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", "0.71em")
+      .attr("text-anchor", "end")
+      .text("Price ($)");
+    g.append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-linejoin", "round")
+      .attr("stroke-linecap", "round")
+      .attr("stroke-width", 1.5)
+      .attr("d", line);
+  }
   ngOnInit() {
     this.initMap();
     this.bar();
     this.httpClient.get<any>('./assets/chcg.json').subscribe(data => {
       this.barChart(data.features);
     });
+    this.httpClient.get<any>('https://api.coindesk.com/v1/bpi/historical/close.json?start=2018-04-01&end=2018-04-30').subscribe(data => {
+      this.lineChart(d3.entries(data.bpi));
+    })
 
   }
 
